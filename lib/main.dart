3ed9d7d6df61acfc9268c80e6a3e44f606a7c485
unknown <oseji2augustina@gmail.com>
2022-10-21 14:33:55 +0100

    i have committed my work

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(const Assessment());
+}
+
+class Assessment extends StatelessWidget {
+  const Assessment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            drawer: Drawer(
+              child: ListView(
+                children: const [
+                  UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(color: Colors.red),
+                    accountName: Text("OSEJI O. AUGUSTINA"),
+                    accountEmail: Text("Oseji2augustina@gmaail.com"),
+                    currentAccountPicture: Icon(Icons.person),
+                  )
+                ],
+              ),
+            ),
+            appBar: AppBar(),
+            body: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Container(
+                  height: 510,
+                  width: 510,
+                  decoration: const BoxDecoration(color: Colors.blueGrey),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            height: 235,
+                            width: 235,
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 15, color: Colors.black54),
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(20))),
+                            child: Image.asset(
+                              "assets/images/pic3.jpg",
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          Container(
+                            height: 235,
+                            width: 235,
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 15, color: Colors.black54),
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(20))),
+                            child: Image.asset(
+                              "assets/images/pic2.jpg",
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              height: 235,
+                              width: 235,
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 15, color: Colors.black54),
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(20))),
+                              child: Image.asset(
+                                "assets/images/pic7.jpg",
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            Container(
+                              height: 235,
+                              width: 235,
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 15, color: Colors.black54),
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(20))),
+                              child: Image.asset(
+                                "assets/images/pic9.jpg",
+                                fit: BoxFit.fill,
+                              ),
+                            )
+                          ]),
+                    ],
+                  ),
+                ))));
+  }
+}
